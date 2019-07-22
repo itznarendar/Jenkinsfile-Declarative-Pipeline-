ac21f5263046f2bf871b6772143996acfefd1753
itznarendar <itznarendar@gmail.com>
2019-07-22 21:02:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
 
+    static int print(){
+        return 9999
+    }
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        print();
+             System.out.println("Hello, World");
     }
 
 }
